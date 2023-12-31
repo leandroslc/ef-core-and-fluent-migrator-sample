25ca34490b1ae929bd14bcc2ca477226feb58c74
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2023-12-31 13:23:16 -0300

    feat: Music models

---
+++ src/Core/Musics/Style.cs
@@ -0,0 +1,8 @@
+namespace EFCoreAndFluentMigrator.Core.Musics;
+
+public class Style
+{
+    public int Id { get; private set; }
+
+    public required string Name { get; init; }
+}
---
+++ src/Core/Musics/Work.cs
@@ -0,0 +1,16 @@
+namespace EFCoreAndFluentMigrator.Core.Musics;
+
+public class Work
+{
+    public int Id { get; private set; }
+
+    public required string Name { get; init; }
+
+    public required DateTime CompositionYear { get; init; }
+
+    public required string CatalogNumber { get; init; }
+
+    public required Style Style { get; init; }
+
+    public required Compositor Compositor { get; init; }
+}

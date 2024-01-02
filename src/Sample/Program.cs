f1cf114c15e1e5abc1c1445a77963aef624383bf
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2024-01-01 22:31:18 -0300

    feat: Read connection string from appsettings.json

--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -2,17 +2,14 @@
 using EFCoreAndFluentMigrator.Sample;
 using EFCoreAndFluentMigrator.Sample.Configuration;
 using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-// It is also possible to use a ConfigurationBuilder here
-var connectionString =
-@"
-    Server=127.0.0.1;
-    Port=5432;
-    Database=archives;
-    User Id=postgres;
-    Password=80d476dddb5d4d799b38f6d9ef44c17f;
-";
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false, true)
+    .Build();
+
+var connectionString = configuration.GetConnectionString("Test")!;
 
 // Configure services
 var services = new ServiceCollection()
